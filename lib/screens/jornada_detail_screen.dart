76d944325f87e686342022f9f7dee2be7ec3f990
khleoalba-blip <khleoalba@gmail.com>
2026-06-24 11:01:18 -0600

    cleanup: remover import no usado de intl en jornada_detail_screen

--- lib/screens/jornada_detail_screen.dart
+++ lib/screens/jornada_detail_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import '../models/jornada_model.dart';
 import '../providers/jornadas_provider.dart';
